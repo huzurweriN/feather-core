b540ec6bf6f2d8ad86838e62c1fd6a0182e276e9
huzurweriN <136732486+huzurweriN@users.noreply.github.com>
2023-08-16 19:34:42 +0300

    Create tr_tr.lua

---
+++ locale/tr_tr.lua
@@ -0,0 +1,6 @@
+LocalesAPI.register("tr_tr", {
+    loadscreen_title = "Dunya Yukleniyor...",
+    loadscreen_subtitle = "Bu tozlu sınırda, yükleme anları kamp ateşlerinin yakınında fısıldadı masallar.",
+    loadscreen_signature = "-Muhtemelen biri",
+    spawn_welcome = "Feathercore'a hoş geldiniz!"
+})
